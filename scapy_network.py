be0f32ea6f0af8d3d8c75c9126f77e27e78d6893
keyblue <liujiakang@petalmail.com>
2023-05-30 19:12:49 +0800

    更新

--- scapy_network.py
+++ scapy_network.py
@@ -1,33 +1,19 @@
 # -*- coding:utf-8 -*-
 from scapy.all import *
-from random import randint
-import sys
-import multiprocessing
-import ipaddress
 from scapy.layers.inet import ICMP, IP
-from scapy.layers.l2 import Ether
 import time
 
 
-def scapy_ping_one(host):
-    id_ip = randint(1, 65535)
-    id_ping = randint(1, 65535)
-    seq_ping = randint(1, 65535)
-    packet = IP(dst=host, ttl=128, id=id_ip) / ICMP(id=id_ping, seq=seq_ping)
-    ping = sr1(packet, timeout=0.2, verbose=False)
-    if ping:
-        os._exit(3)
-
-
 def scapy_ping(ip):
-    ping_one = multiprocessing.Process(target=scapy_ping_one, args=(ip,))
-    ping_one.start()
-    time.sleep(0.3)
-    if ping_one.exitcode == 3:
-        return True
-    else:
-        ping_one.terminate()
-    return False
+    loss = 0
+    for i in range(4):
+        packet = IP(dst=ip, ttl=128) / ICMP()
+        ping = sr1(packet, timeout=0.2, verbose=False)
+        if not ping:
+            loss += 1
+    # 丢包率
+    probability = str(loss/4) + '%'
+    return True, probability
 
 
 if __name__ == '__main__':
