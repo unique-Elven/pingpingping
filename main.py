b689862d7bda1b461032d26a507b3674ed0cacc6
keyblue <liujiakang@petalmail.com>
2023-05-31 15:00:58 +0800

    更新

--- main.py
+++ main.py
@@ -1,17 +1,36 @@
-import pygame
-import sys
+import threading
+from tkinter import *
+from tkinter import messagebox
 
-# 初始化pygame
-pygame.init()
-# 获取对显示系统的访问，并创建一个窗口screen
-# 窗口大小为670x670
-screen = pygame.display.set_mode((670, 670))
-screen_color = [238, 154, 73]  # 设置画布颜色,[238,154,73]对应为棕黄色
-line_color = [0, 0, 0]  # 设置线条颜色，[0,0,0]对应黑色
 
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            sys.exit()
+class Color:
+    # 自定义颜色
+    ACHIEVEMENT = (220, 160, 87)
+    VERSION = (220, 160, 87)
+    # 固定颜色
+    BLACK = (0, 0, 0)
+    WHITE = (255, 255, 255)
+    RED = (255, 0, 0)
+    GREEN = (0, 255, 0)
+    BLUE = (0, 0, 255)
+    GREY = (128, 128, 128)  # 中性灰
+    TRANSPARENT = (255, 255, 255, 0)  # 白色的完全透明
 
-    pygame.display.update()
+
+class Interface:
+    def __init__(self):
+        self.root = Tk()
+        self.root.config(bg='#CCCCCC')
+        self.root.title("PingPingPing")
+        self.root.geometry('350x220')
+
+    def basic_background(self):
+        pass
+
+    def mainloop(self):
+        self.root.mainloop()
+
+
+if __name__ == '__main__':
+    game = Interface()
+    game.mainloop()
--- scapy_network.py
+++ scapy_network.py
@@ -7,8 +7,8 @@ import time
 def scapy_ping(ip):
     loss = 0
     for i in range(4):
-        packet = IP(dst=ip, ttl=128) / ICMP()
-        ping = sr1(packet, timeout=0.2, verbose=False)
+        packets = IP(dst=ip, ttl=128) / ICMP()
+        ping = sr1(packets, timeout=0.2, verbose=False)
         if not ping:
             print('no')
             loss += 1
