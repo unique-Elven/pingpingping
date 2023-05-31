1fc90702e26cfa73753d86e1bd9ecfea06e17d8a
keyblue <liujiakang@petalmail.com>
2023-05-31 16:58:52 +0800

    更新

--- main.py
+++ main.py
@@ -4,34 +4,37 @@ from tkinter import messagebox
 
 
 class Color:
-    # 自定义颜色
-    ACHIEVEMENT = (220, 160, 87)
-    VERSION = (220, 160, 87)
-    # 固定颜色
-    BLACK = (0, 0, 0)
-    WHITE = (255, 255, 255)
-    RED = (255, 0, 0)
-    GREEN = (0, 255, 0)
-    BLUE = (0, 0, 255)
-    GREY = (128, 128, 128)  # 中性灰
-    TRANSPARENT = (255, 255, 255, 0)  # 白色的完全透明
+    LINEN = '#FAF0E6'  # Linen 亚麻布
+    GREY = '#D3D3D3'  # D3D3D3 LightGrey 浅灰色
+    SILVER = '#C0C0C0'  # C0C0C0 Silver 银灰色
+    BLACK = '#000000'  # 000000 Black 纯黑
+    SNOW = '#FFFAFA'  # FFFAFA Snow 雪白色
+    WHITE = '#FFFFFF'  # White 纯白色
 
 
-class Interface:
+class MyGui:
     def __init__(self, windows):
         self.windows = windows
-        self.windows.config(bg='#CCCCCC')
+
+    def set_windows(self):
+        root.resizable(False, False)
+        self.windows.config(bg=Color.WHITE)
         self.windows.title("PingPingPing")
-        self.windows.geometry('350x220')
+        self.windows.geometry('500x350')
+        self.pingE = Entry(root, font=('Times', 15))
+        self.pingE.place(x=125, y=120, width=200, height=35)
+        self.pingB = Button(root, text="Ping", font=("Times", 15), bg='#DDDDDD')
+        self.pingB.place(x=325, y=120, width=60, height=35)
 
     def basic_background(self):
         pass
 
     def run(self):
+        self.set_windows()
         self.windows.mainloop()
 
 
 if __name__ == '__main__':
     root = Tk()
-    gui = Interface(root)
+    gui = MyGui(root)
     gui.run()
